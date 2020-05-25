a71912c4458665cdbb1277cad6f28fe9bf54a2db
mdb571 <r.mohammedbilal@gmail.com>
2020-05-25 16:01:43 +0530

    added new api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import requests,json,os
+import requests
+import json
+import os
 from datetime import date
 import datetime
-from flask import Flask,request
+from flask import Flask, request
 from twilio.rest import Client
 from twilio import twiml
 
@@ -10,54 +12,52 @@ from twilio import twiml
 # DANGER! This is insecure. See http://twil.io/secure
 ACCOUNT_SID =os.environ['ACCOUNT_SID']
 AUTH_TOKEN = os.environ['AUTH_TOKEN']
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+client = Client(ACCOUNT_SID,AUTH_TOKEN)
 
 
-app=Flask(__name__)
-       
+app = Flask(__name__)
+
+
 def get_status():
-    today=date.today().strftime("%d-%B-%y") 
-    previous_day=(datetime.date.today()-datetime.timedelta(1)).strftime("%d-%B-%y")
-    data = requests.get('https://api.covid19india.org/states_daily.json').json()
+    today = date.today().strftime("%d-%B-%y")
+    previous_day = (datetime.date.today() -
+                    datetime.timedelta(1)).strftime("%d-%B-%y")
+    data = requests.get(
+        'https://api.covid19india.org/states_daily.json').json()
+    data2 = requests.get('https://api.track-covid19.in/reports_v2.json').json()
 
-    for _ in reversed(data['states_daily']):
-        if _['date']==today:
-            if _['status']=="Confirmed":
-                confirmed=_['kl']
-            if _['status']=="Recovered":
-                recovered=_['kl']
-            if _['status']=="Deceased":
-                deceased=_['kl']
-            return('\nCovid Stats-Kerala on {} \n Confirmed: {} \n Recovered: {} \n Deceased: {}'.format(today,confirmed,recovered,deceased))
-        
-        elif _['date']==previous_day:
-            if _['status']=="Confirmed":
-                confirmed=_['kl']
-            if _['status']=="Recovered":
-                recovered=_['kl']
-            if _['status']=="Deceased":
-                deceased=_['kl']
-            return('\nCovid Stats-Kerala on {} \n Confirmed: {} \n Recovered: {} \n Deceased: {}'.format(previous_day,confirmed,recovered,deceased))
+    if data2['states']['KL']['today']['confirmed'] != 0:
+        updated=data2['updatedTime']
+        confirmed=data2['states']['KL']['today']['confirmed']
+        recovered=data2['states']['KL']['today']['recovered']
+        deceased=data2['states']['KL']['today']['dead']
+        return('\nCovid Stats-Kerala on {} \n Confirmed: {} \n Recovered: {} \n Deceased: {} \n Last Updated: {} '.format(today, confirmed, recovered, deceased,updated))
 
+    else:
+        for _ in data['states_daily'][-3:]:
+            if _['date'] == previous_day:
+                if _['status'] == "Confirmed":
+                    confirmed = _['kl']
+                    continue
+                if _['status'] == "Recovered":
+                    recovered = _['kl']
+                    continue                
+                if _['status'] == "Deceased":
+                    deceased = _['kl']
+                return('\nCovid Stats-Kerala on {} \n Confirmed: {} \n Recovered: {} \n Deceased: {} \n Last Updated: {} 23:59:59 '.format(previous_day,confirmed,recovered,deceased,previous_day))
 
-    # if str(data['updatedTime'][:10])==today:
-    #     confirmed=data['states']['KL']['today']['confirmed']
-    #     recovered=data['states']['KL']['today']['recovered']
-    #     deceased=data['states']['KL']['today']['dead']
+   
 
-    return('\nCovid Stats-Kerala on {} \n Confirmed: {} \n Recovered: {} \n Deceased: {}'.format(today,confirmed,recovered,deceased))
-
-
-@app.route('/update',methods=['GET'])
+@app.route('/update', methods=['GET'])
 def inbound_sms():
 
     message = client.messages \
-                .create(
-                     body=get_status(),
-                     from_='+18706864040', #add your twilio no. here
-                     to='+919995153948' #add your verified number here
-                 )
+        .create(
+            body=get_status(),
+            from_='+18706864040',  # add your twilio no. here
+            to='+919995153948'  # add your verified number here
+        )
     print(message.sid)
     return("Your request was send")
-
 
--- schedule.py
+++ schedule.py
@@ -1,6 +1,10 @@
 import requests
 
 def get_updates():
-    refresh=requests.get('host.domain.com/update') #update  with your domain 
+    refresh=requests.get('host.domain.com/update') #update  with your domain
 
 get_updates()
+
+
+
+
